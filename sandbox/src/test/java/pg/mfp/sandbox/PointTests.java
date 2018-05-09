ea29f6e771cdb4431f93ae14615b9b49b0f4c7a4
Анна Вихристюк <avihristuk@jetmoney.ru>
2018-05-09 23:16:31 +0300

    Исправление тестов

--- sandbox/src/test/java/pg/mfp/sandbox/PointTests.java
+++ sandbox/src/test/java/pg/mfp/sandbox/PointTests.java
@@ -14,101 +14,141 @@ public class PointTests {
 
   @Test
   public void testDistance1() {
-    distanceCalculation(2, 2, 4, 4, "2,83");
+    Point p1 = new Point(2, 2);
+    Point p2 = new Point (4, 4);
+    Assert.assertEquals(p1.distance(p2), "2,83");
   }
 
   @Test
   public void testDistance2() {
-    distanceCalculation(-2, -2, -4, -4, "2,83");
+    Point p1 = new Point(-2, -2);
+    Point p2 = new Point (-4, -4);
+    Assert.assertEquals(p1.distance(p2), "2,83");
   }
 
   @Test
   public void testDistance3() {
-    distanceCalculation(-2, 2, 4, 4, "6,32");
+    Point p1 = new Point(-2, 2);
+    Point p2 = new Point (4, 4);
+    Assert.assertEquals(p1.distance(p2), "6,32");
   }
 
   @Test
   public void testDistance4() {
-    distanceCalculation(2, -2, 4, 4, "6,32");
+    Point p1 = new Point(2, -2);
+    Point p2 = new Point (4, 4);
+    Assert.assertEquals(p1.distance(p2), "6,32");
   }
 
   @Test
   public void testDistance5() {
-    distanceCalculation(2, 2, -4, 4, "6,32");
+    Point p1 = new Point(2, 2);
+    Point p2 = new Point (-4, 4);
+    Assert.assertEquals(p1.distance(p2), "6,32");
   }
 
   @Test
   public void testDistance6() {
-    distanceCalculation(2, 2, 4, -4, "6,32");
+    Point p1 = new Point(2, 2);
+    Point p2 = new Point (4, -4);
+    Assert.assertEquals(p1.distance(p2), "6,32");
   }
 
   @Test
   public void testDistance7() {
-    distanceCalculation(-2, -2, 4, 4, "8,49");
+    Point p1 = new Point(-2, -2);
+    Point p2 = new Point (4, 4);
+    Assert.assertEquals(p1.distance(p2), "8,49");
   }
 
   @Test
   public void testDistance8() {
-    distanceCalculation(2, 2, -4, -4, "8,49");
+    Point p1 = new Point(2, 2);
+    Point p2 = new Point (-4, -4);
+    Assert.assertEquals(p1.distance(p2), "8,49");
   }
 
   @Test
   public void testDistance9() {
-    distanceCalculation(-2, 2, 4, -4, "8,49");
+    Point p1 = new Point(-2, 2);
+    Point p2 = new Point (4, -4);
+    Assert.assertEquals(p1.distance(p2), "8,49");
   }
 
   @Test
   public void testDistance10() {
-    distanceCalculation(2, -2, -4, 4, "8,49");
+    Point p1 = new Point(2, -2);
+    Point p2 = new Point (-4, 4);
+    Assert.assertEquals(p1.distance(p2), "8,49");
   }
 
   @Test
   public void testDistance11() {
-    distanceCalculation(-999999999, -999999999, 999999999, 999999999, "2828427121,92");
+    Point p1 = new Point(-999999999, -999999999);
+    Point p2 = new Point (999999999, 999999999);
+    Assert.assertEquals(p1.distance(p2), "2828427121,92");
   }
 
   @Test
   public void testDistance12() {
-    distanceCalculation(0, 0, 200, 200, "282,84");
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point (200, 200);
+    Assert.assertEquals(p1.distance(p2), "282,84");
   }
 
   @Test
   public void testDistance13() {
-    distanceCalculation(200, 200, 0, 0, "282,84");
+    Point p1 = new Point(200, 200);
+    Point p2 = new Point (0, 0);
+    Assert.assertEquals(p1.distance(p2), "282,84");
   }
 
   @Test
   public void testDistance14() {
-    distanceCalculation(0, 0, 0, 0, "0,00");
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point (0, 0);
+    Assert.assertEquals(p1.distance(p2), "0,00");
   }
 
   @Test
   public void testDistance15() {
-    distanceCalculation(0.123, 0.456, 0.678, 0.89, "0,70");
+    Point p1 = new Point(0.123, 0.456);
+    Point p2 = new Point (0.678, 0.89);
+    Assert.assertEquals(p1.distance(p2), "0,70");
   }
 
   @Test
   public void testDistance16() {
-    distanceCalculation(123.123, 456.456, 678.678, 890.89, "705,25");
+    Point p1 = new Point(123.123, 456.456);
+    Point p2 = new Point (678.678, 890.89);
+    Assert.assertEquals(p1.distance(p2), "705,25");
   }
 
   @Test
   public void testDistance17() {
-    distanceCalculation(150, 1, 150, -1, "2,00");
+    Point p1 = new Point(150, 1);
+    Point p2 = new Point (150, -1);
+    Assert.assertEquals(p1.distance(p2), "2,00");
   }
 
   @Test
   public void testDistance18() {
-    distanceCalculation(-150, 1, 150, 1, "300,00");
+    Point p1 = new Point(-150, 1);
+    Point p2 = new Point (150, 1);
+    Assert.assertEquals(p1.distance(p2), "300,00");
   }
 
   @Test
   public void testDistance19() {
-    distanceCalculation(-150, 1, -150, -1, "2,00");
+    Point p1 = new Point(-150, 1);
+    Point p2 = new Point (-150, -1);
+    Assert.assertEquals(p1.distance(p2), "2,00");
   }
 
   @Test
   public void testDistance20() {
-    distanceCalculation(-150, -1, 150, -1, "300,00");
+    Point p1 = new Point(-150, -1);
+    Point p2 = new Point (150, -1);
+    Assert.assertEquals(p1.distance(p2), "300,00");
   }
 }
